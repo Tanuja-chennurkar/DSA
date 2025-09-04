5251756dbc048096e863d6683b7f4a3af07ad10a
Tanuja-chennurkar <109684064+Tanuja-chennurkar@users.noreply.github.com>
2025-09-04 15:08:44 +0530

    [1081] [Time Beats: 100.00%] [Memory Beats: 82.89%] - LeetPush

---
+++ Smallest-Subsequence-of-Distinct-Characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        
+
+        vector<int> frequency(26,0);
+        vector<bool> instack(26,false);
+        stack<char> st;
+
+        for(char c: s){
+            frequency[c-'a']++;
+        }
+        for(char c:s){
+            frequency[c-'a']--;
+
+            if(instack[c-'a']) continue;
+
+            while(!st.empty()&&st.top()>c&&frequency[st.top()-'a']>0){
+                instack[st.top()-'a']=false;
+                st.pop();
+            }
+            st.push(c);
+            instack[c-'a']=true;
+        }
+
+        string result="";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+    
+};
