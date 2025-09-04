49f8901e73bd9664dd009dcd2b468ec543c223f6
Tanuja-chennurkar <109684064+Tanuja-chennurkar@users.noreply.github.com>
2025-09-04 14:01:59 +0530

    [503] [Time Beats: 60.77%] [Memory Beats: 82.40%] - LeetPush

---
+++ Next-Greater-Element-II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st; 
+        int n=nums.size();
+        vector<int> result(n,-1);
+
+        for(int i=0;i<2*n;i++){
+            int num=nums[i%n];
+            while(!st.empty()&&nums[st.top()]<num){
+                result[st.top()]=num;
+                st.pop();
+            }
+            if(i<n) st.push(i);
+        }
+    return result;
+    }
+};
