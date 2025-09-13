5d056fe2e8159bf4f566627509b97715a572fc09
Tanuja-chennurkar <109684064+Tanuja-chennurkar@users.noreply.github.com>
2025-09-13 19:22:51 +0530

    [392] [Time Beats: 100.00%] [Memory Beats: 66.79%] - LeetPush

---
+++ Is-Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=t.size();
+        int m=s.size();
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(t[i]==s[j]){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return j==m;
+    }
+};
