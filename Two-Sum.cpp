c826433f6e2c4e3e7deed54d928ae103fc6c4d6c
Tanuja-chennurkar <109684064+Tanuja-chennurkar@users.noreply.github.com>
2025-09-13 17:05:10 +0530

    [1] [Time Beats: 100.00%] [Memory Beats: 20.26%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> hashmap;
+         unordered_map<int,int> hashmap;
 
         for(int i=0;i<nums.size();i++){
             int c= target-nums[i];
@@ -12,4 +12,5 @@ public:
         }
         return {};
     }
+    
 };
