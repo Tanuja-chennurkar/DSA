c053811918458f18b2e6773b9e439e81184f3265
Tanuja-chennurkar <109684064+Tanuja-chennurkar@users.noreply.github.com>
2026-02-03 12:34:22 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 44.53%] - LeetPush

--- House-Robber.cpp
+++ House-Robber.cpp
@@ -1,14 +1,16 @@
-class Solution {
-public:
-    int rob(vector<int>& nums) {
-       int a =0;
-       int b=0;
-
-       for(int num:nums){
-        int temp=max(a,b+num);
-        b=a;
-        a=temp;
-       }
-       return a;
-    }
-};
+1class Solution {
+2public:
+3    int rob(vector<int>& nums) {
+4        int n=nums.size();
+5        vector<int> dp(n);
+6        if(n==1) return nums[0];
+7
+8        dp[0]=nums[0];
+9        dp[1]=max(nums[0],nums[1]);
+10
+11        for(int i=2;i<n;i++){
+12            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+13        }
+14        return dp[n-1];
+15    }
+16};
